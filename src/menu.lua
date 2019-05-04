f96c4859441e8501351621c467338cee507483b5
bambalic <47799410+bambalic@users.noreply.github.com>
2019-05-04 23:42:21 +0200

    deleted unecessary, fixed some stuff

--- src/menu.lua
+++ src/menu.lua
@@ -1,5 +1,6 @@
---V=require "values"
-I=require "img"
+
+I=require "imgEnemy"
+
 local M={}
 
 
@@ -10,88 +11,11 @@ M.spawnSpeed=200
 
 local frames={}
 local currentFrameEnemies=1
--- pocetna brzina enemies
---E.enemySpeed=50
 
--- vreme nakon kojeg se povecava brzina enemies
---local enemySpeedCooldown=500
 
---frames[1]=love.graphics.newImage("small/a10015.png")
---frames[2]=love.graphics.newImage("small/a10014.png")
---frames[3]=love.graphics.newImage("small/a10013.png")
---frames[4]=love.graphics.newImage("small/a10012.png")
---frames[5]=love.graphics.newImage("small/a10011.png")
---frames[6]=love.graphics.newImage("small/a10010.png")
---frames[7]=love.graphics.newImage("small/a10009.png")
---frames[8]=love.graphics.newImage("small/a10008.png")
---frames[9]=love.graphics.newImage("small/a10007.png")
---frames[10]=love.graphics.newImage("small/a10006.png")
---frames[11]=love.graphics.newImage("small/a10005.png")
---frames[12]=love.graphics.newImage("small/a10004.png")
---frames[13]=love.graphics.newImage("small/a10003.png")
---frames[14]=love.graphics.newImage("small/a10002.png")
---frames[15]=love.graphics.newImage("small/a10001.png")
---frames[16]=love.graphics.newImage("small/a10000.png")
 
--------------------------------------------------
--- ponavljanje istog load-a (u menu i enemy) premesteno u svoj module
 
---frames[1]={}
---frames[2]={}
---frames[3]={}
---
---frames[1][1]=love.graphics.newImage("small/a10015.png")
---frames[1][2]=love.graphics.newImage("small/a10014.png")
---frames[1][3]=love.graphics.newImage("small/a10013.png")
---frames[1][4]=love.graphics.newImage("small/a10012.png")
---frames[1][5]=love.graphics.newImage("small/a10011.png")
---frames[1][6]=love.graphics.newImage("small/a10010.png")
---frames[1][7]=love.graphics.newImage("small/a10009.png")
---frames[1][8]=love.graphics.newImage("small/a10008.png")
---frames[1][9]=love.graphics.newImage("small/a10007.png")
---frames[1][10]=love.graphics.newImage("small/a10006.png")
---frames[1][11]=love.graphics.newImage("small/a10005.png")
---frames[1][12]=love.graphics.newImage("small/a10004.png")
---frames[1][13]=love.graphics.newImage("small/a10003.png")
---frames[1][14]=love.graphics.newImage("small/a10002.png")
---frames[1][15]=love.graphics.newImage("small/a10001.png")
---frames[1][16]=love.graphics.newImage("small/a10000.png")
---
---frames[2][1]=love.graphics.newImage("small/a30015.png")
---frames[2][2]=love.graphics.newImage("small/a30014.png")
---frames[2][3]=love.graphics.newImage("small/a30013.png")
---frames[2][4]=love.graphics.newImage("small/a30012.png")
---frames[2][5]=love.graphics.newImage("small/a30011.png")
---frames[2][6]=love.graphics.newImage("small/a30010.png")
---frames[2][7]=love.graphics.newImage("small/a30009.png")
---frames[2][8]=love.graphics.newImage("small/a30008.png")
---frames[2][9]=love.graphics.newImage("small/a30007.png")
---frames[2][10]=love.graphics.newImage("small/a30006.png")
---frames[2][11]=love.graphics.newImage("small/a30005.png")
---frames[2][12]=love.graphics.newImage("small/a30004.png")
---frames[2][13]=love.graphics.newImage("small/a30003.png")
---frames[2][14]=love.graphics.newImage("small/a30002.png")
---frames[2][15]=love.graphics.newImage("small/a30001.png")
---frames[2][16]=love.graphics.newImage("small/a30000.png")
---
---frames[3][1]=love.graphics.newImage("small/a40015.png")
---frames[3][2]=love.graphics.newImage("small/a40014.png")
---frames[3][3]=love.graphics.newImage("small/a40013.png")
---frames[3][4]=love.graphics.newImage("small/a40012.png")
---frames[3][5]=love.graphics.newImage("small/a40011.png")
---frames[3][6]=love.graphics.newImage("small/a40010.png")
---frames[3][7]=love.graphics.newImage("small/a40009.png")
---frames[3][8]=love.graphics.newImage("small/a40008.png")
---frames[3][9]=love.graphics.newImage("small/a40007.png")
---frames[3][10]=love.graphics.newImage("small/a40006.png")
---frames[3][11]=love.graphics.newImage("small/a40005.png")
---frames[3][12]=love.graphics.newImage("small/a40004.png")
---frames[3][13]=love.graphics.newImage("small/a40003.png")
---frames[3][14]=love.graphics.newImage("small/a40002.png")
---frames[3][15]=love.graphics.newImage("small/a40001.png")
---frames[3][16]=love.graphics.newImage("small/a40000.png")
 
-------------------------------------------------------------------
 
 
 local background=love.graphics.newImage("ironmanb1.png")
@@ -148,10 +72,7 @@ function M:draw()
     love.graphics.draw(backgroundSide,-5,-5) -- -5 zbog toga sto se vidi pozadina ako je 0
     love.graphics.draw(backgroundSide,620,-5)
     for i,e in pairs(M.enemiesAll.enemies) do
-        --        love.graphics.rectangle("fill",e.x,e.y,enemy.size,enemy.size)
-        --        love.graphics.draw(activeFrame,e.x,e.y)
-        --        love.graphics.draw(activeFrame,e.x-30,e.y)
-        --love.graphics.draw(frames[currentFrameEnemies],e.x-30,e.y)
+
         love.graphics.draw(I.frames[e.color][currentFrameEnemies],e.x-30,e.y)
     end
     love.graphics.draw(background,175,0)
--- src/player.lua
+++ src/player.lua
@@ -1,5 +1,4 @@
---E=require "enemy"
---C=require "checks"
+
 V=require "values"
 
 local P={}
@@ -7,37 +6,23 @@ local P={}
 
 
 
---scoreBullets=0
---bulletsUsed=0
-----bulletSpeed=400
---maxBulletSpeed=1000
---minBulletSpeed=200
---maxEnemySpeed=700
---minEnemySpeed=100
---minBulletCooldown=5
-
-
---local poravnanjePlayerX=50
---local poravnanjePlayerY=0
---local poravnanjeBulletX=140
---local poravnanjeBulletY=30
 
 
 -- indikator jaceg bullet-a
 local strongerBullet=0
---P.bulletSpeed=1
+
 local frame
 
 picGore=love.graphics.newImage("iron_gore.png")
 picDole=love.graphics.newImage("iron_dole.png")
 picPucanje1=love.graphics.newImage("iron_pucanje1.png")
 picPucanje2=love.graphics.newImage("iron_pucanje222222.png")
-picIzmedjuPucanja=love.graphics.newImage("iron_izmedjupucanja.png")
 bulletPic=love.graphics.newImage("bullet1.png")
 bulletPicStrong=love.graphics.newImage("bulletstrong.png")
 picSuper=love.graphics.newImage("iron_strong.png")
+picBoom=love.graphics.newImage("boom.png")
+
 frame=picGore
---bulletCooldown=20
 
 P.superPowerCooldown=0
 P.superPowerCooldownIspis="AVAILABLE"
@@ -45,88 +30,26 @@ P.superPowerCooldownIspis="AVAILABLE"
 
 local currentFrame=1
 
---local width=love.graphics.getWidth()
---local height=love.graphics.getHeight()
+
+
 
 P.player={}
 
---P.player.cooldown=20
+
 P.player.size=100
 P.player.x=0+V.poravnanjePlayerX
 P.player.y=V.height/2-P.player.size/2-V.poravnanjePlayerY
 P.player.speed=400
 P.player.bullets={}
---P.player.cooldownNew=20
---    player.bulletSpeed=400
+
+
 
 
 P.player.fire=function()
     if V.playerCooldown<=0 then
         V.playerCooldown=V.playerCooldownNew
 
-        -- sa frame-om pre pucanja
-        -- promeniti if currentFrame==3 or currentFrame==5
-        --if currentFrame==1 or currentFrame==5 then
-        --    currentFrame=2
-        --    frame=picPrePucanja
-        --elseif currentFrame==2 then
-        --    currentFrame=3
-        --    frame=picPucanje1
-        --elseif currentFrame==3 then
-        --    currentFrame=4
-        --    frame=picIzmedjuPucanja
-        --elseif currentFrame==4 then
-        --    currentFrame=5
-        --    frame=picPucanje2
-        --end
 
-        --ova-sa frame-om izmedju pucanja samo pre pucanja1 (nakon pucanja2)
-        -- promeniti if currentFrame==2 or currentFrame==4
-        --if currentFrame==1 or currentFrame==4 then
-        --    currentFrame=2
-        --    frame=picPucanje1
-        --elseif currentFrame==2 then
-        --    currentFrame=3
-        --    frame=picIzmedjuPucanja
-        --elseif currentFrame==3 then
-        --    currentFrame=4
-        --    frame=picPucanje2
-        --end
-
-        ----sa frame-om pre pucanja i pre pucanja1 i pre pucanja2
-        --if currentFrame==1 or currentFrame==5 then
-        --    currentFrame=2
-        --    frame=picIzmedjuPucanja
-        --elseif currentFrame==2 then
-        --    currentFrame=3
-        --    frame=picPucanje1
-        --elseif currentFrame==3 then
-        --    currentFrame=4
-        --    frame=picIzmedjuPucanja
-        --elseif currentFrame==4 then
-        --    currentFrame=5
-        --    frame=picPucanje2
-        --end
-
-
-        --ova-sa frame-om pre pucanja izmedju pucanja1 i pucanja2
-        -- promeniti if currentFrame==2 or currentFrame==4
-        --if currentFrame==1 then
-        --    currentFrame=2
-        --    frame=picPucanje1
-        --elseif currentFrame==2 then
-        --    currentFrame=3
-        --    frame=picIzmedjuPucanja
-        --elseif currentFrame==3 then
-        --    currentFrame=4
-        --    frame=picPucanje2
-        --elseif currentFrame==4 then
-        --    currentFrame=1
-        --    frame=picIzmedjuPucanja
-        --end
-
-        -- bez frame-a pre pucanja
-        -- promeniti if currentFrame==2 or currentFrame==4 izbaciti
         if currentFrame==1 then
             currentFrame=2
             frame=picPucanje1
@@ -135,7 +58,8 @@ P.player.fire=function()
             frame=picPucanje2
         end
 
-        --if currentFrame==2 or currentFrame==4 then
+
+
 
         local bullet={}
 
@@ -155,10 +79,12 @@ P.player.fire=function()
         end
         bullet.x=P.player.x+V.poravnanjeBulletX
         bullet.y=P.player.y+P.player.size/2-bullet.height/2-V.poravnanjeBulletY
-        --bulletsUsed=bulletsUsed+1
+
         table.insert(P.player.bullets,bullet)
-        --end
+
+        return true
     end
+    return false
 end
 
 
@@ -179,9 +105,15 @@ function P:update(dt)
         end
     end
 
+
     if love.keyboard.isDown("space") then
-        P.player:fire()
+        if(P.player:fire()==true) then
+            S.saber2:stop()
+            S.saber2:play()
+        end
     end
+
+
 
 
     if P.superPowerCooldown<=0 then
@@ -189,8 +121,9 @@ function P:update(dt)
 
         if love.keyboard.isDown("k") then
             frame=picSuper
-            --        destroyAll(enemiesAll.enemies)
+            S.boomef:play()
 
+            V.superPowerImage=V.superPowerImageNew
             C.destroyAll(E.enemiesAll.enemies)
             P.superPowerCooldown=20
             P.superPowerCooldownIspis="NOT AVAILABLE"
@@ -212,7 +145,7 @@ function P:update(dt)
 
 
 
-
+    V.superPowerImage=V.superPowerImage-10*dt
     V.playerCooldown=V.playerCooldown-50*dt
     if(P.superPowerCooldown>0) then
         P.superPowerCooldown=P.superPowerCooldown-1*dt
@@ -220,7 +153,9 @@ function P:update(dt)
 end
 
 function P.player:draw()
-
+    if V.superPowerImage>0 then
+        love.graphics.draw(picBoom,175, 130)
+    end
     love.graphics.draw(frame,P.player.x,P.player.y,0,0.6)
 
     for i,b in pairs(self.bullets) do
@@ -230,7 +165,6 @@ function P.player:draw()
         else love.graphics.setColor(255,255,255)
             love.graphics.draw(bulletPic,b.x-30,b.y-20)
         end
-
     end
 end
 
